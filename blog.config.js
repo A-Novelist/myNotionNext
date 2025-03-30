373ff44d522557350b92224e400df9e2f2d7da59
A-Novelist <96641526+A-Novelist@users.noreply.github.com>
2025-03-30 10:34:52 +0800

    Update blog.config.js
    4

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,8 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '诗远', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '嵌入式全栈工程师👌', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '三年嵌入式全栈工程师👌', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '· 青年软件工程师👌', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://chuangjingtech.cn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '科技美学 创无止境', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
