d1824f581119f1d3b4fda41de3d1f5b6bd10c38c
A-Novelist <96641526+A-Novelist@users.noreply.github.com>
2025-03-30 10:40:07 +0800

    Update blog.config.js
    5

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,6 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '诗远', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '三年嵌入式全栈工程师👌', // 作者简介
-  BIO: process.env.NEXT_PUBLIC_BIO || '· 青年软件工程师👌', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://chuangjingtech.cn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '科技美学 创无止境', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
