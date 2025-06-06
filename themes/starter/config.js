097556ea0d7dd63625185331a5e8005c527cfcc5
A-Novelist <96641526+A-Novelist@users.noreply.github.com>
2025-06-06 21:21:34 +0800

    Update config.js
    1

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -10,7 +10,7 @@ const CONFIG = {
 
   // 英雄区块导航
   STARTER_HERO_ENABLE: true, // 开启英雄区
-  STARTER_HERO_TITLE_1: '康诗远的个人工作室', // 英雄区文字
+  STARTER_HERO_TITLE_1: '创境科技工作室', // 英雄区文字
   STARTER_HERO_TITLE_2: '科技美学   创无止境', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: '', // 英雄区按钮
